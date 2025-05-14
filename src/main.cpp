a2cb178e94292fba02e83610c9f94d568244c58a
bramlaurens <bramlaurens@gmail.com>
2025-05-14 21:19:26 +0200

    Implemented manual MPPT

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,115 @@
 #include <Arduino.h>
+#define MES_PV 34
+#define MES_SHUNT 33
 
-// put function declarations here:
-int myFunction(int, int);
+// Constants
+#define OP_Gain 12.20
+#define SHUNT_R 0.53
+#define V_offset 0.3
+
+// Prototypes
+float calc_current();
+float calc_voltage();
+float calc_power();
+void pno_algorithm();
+
+// Variables
+float last_voltage = 0;
+float last_power = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(MES_PV, INPUT);
+  pinMode(MES_SHUNT, INPUT);
+  Serial.begin(115200);
+
+  Serial.println("Starting...");
+  delay(1000);
+  Serial.println("Starting... done");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  pno_algorithm();
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+float calc_current(){
+  int raw = analogRead(MES_SHUNT);
+  float voltage_adc = (raw * (3.3 / 4096)) + V_offset;
+  float voltage = voltage_adc / OP_Gain;
+  float current = (voltage / SHUNT_R);
+  return (current);
 }
+
+float calc_voltage(){
+  int raw = analogRead(MES_PV);
+  float voltage = (raw * (3.3 / 4096)) + V_offset;
+  return voltage;
+}
+
+float calc_power(){
+  float voltage = calc_voltage();
+  float current = calc_current();
+  float power = voltage * current;
+  return power;
+}
+
+void pno_algorithm(){
+  float voltage = calc_voltage();
+  float current = calc_current();
+  float power = calc_power();
+
+  Serial.print("Voltage: ");
+  Serial.print(calc_voltage());
+  Serial.print("V");
+  Serial.print("  Current: ");
+  Serial.print(calc_current()*1000);
+  Serial.print("mA");
+  Serial.print("  Power: ");
+  Serial.print(calc_power()*1000);
+  Serial.print("mW");
+
+  
+
+  if ((power - last_power > 0.05) || (power - last_power < -0.05)) {
+
+    Serial.print(" || ");
+
+    if (power > last_power) {
+      // Power increased
+
+      if (voltage > last_voltage) {
+        // Voltage increased
+        Serial.println("Power increased, PV voltage increased. Increase load resistance");
+      }
+      else {
+        // Voltage decreased
+        Serial.println("Power increased, PV voltage decreased. Decrease load resistance");
+      }
+
+    }
+    else {
+      // Power decreased
+      if (voltage > last_voltage) {
+        // Voltage increased
+        Serial.println("Power decreased, PV voltage increased. Decrease load resistance");
+      }
+      else {
+        // Voltage decreased
+        Serial.println("Power decreased, PV voltage decreased. Increase load resistance");
+      }
+    }
+
+  }
+  else {
+    Serial.println(" || ");
+  }
+
+  last_voltage = voltage;
+  last_power = power;
+}
+
+
+
+
+
